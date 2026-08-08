d57572bc6a8a335ce97cb36b47a86d95872977c0
ransombiyato <breadman6961@gmail.com>
2026-08-08 17:21:40 +0530

    Restore ProfileTypeSelectFragment implementation

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ProfileTypeSelectFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ProfileTypeSelectFragment.java
@@ -2,43 +2,29 @@ package net.kdt.pojavlaunch.fragments;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 
 public class ProfileTypeSelectFragment extends Fragment {
-    public static final String TAG = "PROFILE_TYPE_SELECT_FRAGMENT";
-
+    public static final String TAG = "ProfileTypeSelectFragment";
     public ProfileTypeSelectFragment() {
-        super(R.layout.fragment_profile_type_select);
+        super(R.layout.fragment_profile_type);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Button vanilla = view.findViewById(R.id.create_profile_vanilla);
-        Button fabric = view.findViewById(R.id.create_profile_fabric);
-        Button forge = view.findViewById(R.id.create_profile_forge);
-        Button neoforge = view.findViewById(R.id.create_profile_neoforge);
-        Button quilt = view.findViewById(R.id.create_profile_quilt);
-
-        vanilla.setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), VanillaCreateProfileFragment.class, VanillaCreateProfileFragment.TAG, null));
-
-        fabric.setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), FabricCreateProfileFragment.class, FabricCreateProfileFragment.TAG, null));
-
-        forge.setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), ForgeCreateProfileFragment.class, ForgeCreateProfileFragment.TAG, null));
-
-        neoforge.setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), NeoForgeCreateProfileFragment.class, NeoForgeCreateProfileFragment.TAG, null));
-
-        quilt.setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), QuiltCreateProfileFragment.class, QuiltCreateProfileFragment.TAG, null));
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.vanilla_profile).setOnClickListener(v -> Tools.swapFragment(requireActivity(), ProfileEditorFragment.class,
+                ProfileEditorFragment.TAG, false, new Bundle(1)));
+        view.findViewById(R.id.modded_profile_fabric).setOnClickListener((v)->
+                Tools.swapFragment(requireActivity(), FabricInstallFragment.class, FabricInstallFragment.TAG, false, null));
+        view.findViewById(R.id.modded_profile_forge).setOnClickListener((v)->
+                Tools.swapFragment(requireActivity(), ForgeInstallFragment.class, ForgeInstallFragment.TAG, false, null));
     }
 }
