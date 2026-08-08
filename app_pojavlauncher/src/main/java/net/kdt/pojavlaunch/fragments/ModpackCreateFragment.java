8620b41854b36559686264ec152edfd4a6807c96
ransombiyato <breadman6961@gmail.com>
2026-08-08 11:01:26 +0530

    Stub out removed modpack fragment

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModpackCreateFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ModpackCreateFragment.java
@@ -7,23 +7,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import net.kdt.pojavlaunch.R;
-import net.kdt.pojavlaunch.Tools;
-
 public class ModpackCreateFragment extends Fragment {
 
     public static final String TAG = "MODPACK_CREATE_FRAGMENT";
 
     public ModpackCreateFragment() {
-        super(R.layout.fragment_modpack_create);
+        super();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        view.findViewById(R.id.curseforge_button).setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), CurseforgeFragment.class, CurseforgeFragment.TAG, null));
-
-        view.findViewById(R.id.modrinth_button).setOnClickListener(v ->
-                Tools.swapFragment(requireActivity(), ModrinthFragment.class, ModrinthFragment.TAG, null));
+        // Placeholder: modpack installer removed in this fork.
     }
 }
